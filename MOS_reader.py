8f02ff2ac605a6ea569d95fbd99047edc32f0d41
lldcl <daichengliang@gmail.com>
2016-10-27 16:17:00 +0100

    Revert "Revert "test""
    This reverts commit d497e105b408344cfa602f83f275b41fab25ac45.

---
+++ MOS_reader.py
@@ -0,0 +1,69 @@
+"""This program calculates the correlation coefficient between MOS signals and other signals such as O3 and temperature"""
+import os
+import pandas as pd
+import operator
+import numpy as np
+import pylab
+from pylab import *
+from sklearn import linear_model
+from scipy import stats
+
+def readin(path, f_date, cal_file, date_st, date_end):
+        if 'desktop.ini' in cal_file:
+                cal_file.remove('desktop.ini')
+
+        # This bit of code works out which folder the file is in from the name of the file becasue the data is store in folders named 
+        # following the format YYYYMM depending on when the file was recorded.
+        for i in cal_file:
+        # Pick out characters 18 to 21 to correspond to the filename.
+                folder = list(i)[date_st:date_end]
+                f = '20'+"".join(folder)+'/'+i
+
+                #for f in filenames:
+                print (f)
+                #read file into dataframe and call the dataframe data
+                #data = pd.read_csv(path+f, error_bad_lines=False, warn_bad_lines=False)
+                data = pd.read_csv(path+f)
+                        
+
+                # 	convert DAQfactory time into real time pd.datetime object - so it gives time as we would expect, not as a random number.
+                # DAQfactory is the programme we are using to record the data into CSV files.
+                data.TheTime = pd.to_datetime(data.TheTime,unit='D')
+
+                T1 = pd.datetime(1899,12,30,0)
+                T2 = pd.datetime(1970,1,1,0)
+                offset=T1-T2
+                data.TheTime+=offset
+                # Ten second averaging of the raw data
+                Time_avg = '10S'
+                # Make a new copy of the data called mean_resampled 
+                mean_resampled = data.copy(deep=True)
+                mean_resampled.TheTime = pd.to_datetime(mean_resampled.TheTime,unit='L')
+                # Set the index to be the time column, when you do this it drops the index, even though it is set to False.
+                mean_resampled = mean_resampled.set_index(mean_resampled.TheTime,drop=False)
+                mean_resampled = mean_resampled.resample(Time_avg, how='mean',fill_method='pad')
+                # Re-add the time index so that it can be plotted later
+                Time = pd.Series(mean_resampled.index,name='Time', index=mean_resampled.index)
+                mean_resampled = pd.concat([mean_resampled,Time],axis=1)
+                # If there are more than one files to be read in together the data_concat function will join them together.
+                print ('mean_resampled shape = ',mean_resampled.shape)
+                try:
+                        data_concat = data_concat.append(mean_resampled)
+                        print (' concatenating')
+                except NameError:
+                        data_concat = mean_resampled.copy(deep=True)
+                        print (' making data_concat')
+                        
+        # Re-make and re-set the index to be the time column for the data_concat dataframe.
+        T3 = pd.datetime(2015,1,1,0)
+        dt = pd.Series((data_concat.index - T3),index=data_concat.index,name='dt')
+        dt = dt.astype(int64)
+        data_concat = pd.concat([data_concat,dt],axis=1,join_axes=[data_concat.index])
+        stat = 'Y'
+
+        newindex = pd.Series(range(0,data_concat.shape[0]))
+        data_concat = data_concat.set_index(newindex)
+        # Returns the initial date and time that the file began
+        print(data_concat.Time[0])
+        print(data_concat.Time[len(data_concat.Time)-1])
+        return data_concat
---
+++ corrcoef.py
@@ -0,0 +1,311 @@
+"""This program calculates the correlation coefficient between MOS signals and other signals such as O3 and temperature"""
+import os
+import pandas as pd
+import operator
+import numpy as np
+import pylab
+import voc_reader
+import csv
+import MOS_reader as mr
+from pylab import *
+from datetime import datetime
+from sklearn import linear_model
+from scipy import stats
+
+# The path to where the raw files are stored
+path = 'D:/WACL/Data/wacl_data/Raw_data_files/'
+f_date = '201610'
+cal_file = os.listdir(path + f_date +'/MOS')
+# The name of the MOS file to be analysed
+data_concat = mr.readin(path, f_date, cal_file, 1, 5)
+#correlation between MOSc and other signals
+MOS=[ 'MOS1_Av','MOS2_Av','MOS3_Av','MOS4_Av','MOS5_Av','MOS6_Av','MOS7_Av','MOS8_Av']
+print('MOS/NO2_ave1')
+corrcoef_data = []
+for i in MOS:   
+    print(np.corrcoef(data_concat[i],data_concat['NO2_ave1'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['NO2_ave1'])[0][1])
+df = pd.DataFrame(corrcoef_data, columns = ['MOSx/NO2_ave1'])
+
+print('MOS/NO2_ave2')
+corrcoef_data = []
+for i in MOS:   
+    print(np.corrcoef(data_concat[i],data_concat['NO2_ave2'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['NO2_ave2'])[0][1])
+df['MOSx/NO2_ave2'] = corrcoef_data
+
+print('MOS/NO_ave1')
+corrcoef_data = []
+for i in MOS: 
+    print(np.corrcoef(data_concat[i],data_concat['NO_ave1'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['NO_ave1'])[0][1])
+df['MOSx/NO_ave1'] = corrcoef_data
+
+print('MOS/NO_ave2')
+corrcoef_data = []
+for i in MOS: 
+    print(np.corrcoef(data_concat[i],data_concat['NO_ave2'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['NO_ave2'])[0][1])
+df['MOSx/NO_ave2'] = corrcoef_data
+
+print('MOS/CO_ave1')
+corrcoef_data = []
+for i in MOS: 
+    print(np.corrcoef(data_concat[i],data_concat['CO_ave1'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['CO_ave1'])[0][1])
+df['MOSx/CO_ave1'] = corrcoef_data
+
+print('MOS/CO_ave2')
+corrcoef_data = []
+for i in MOS: 
+    print(np.corrcoef(data_concat[i],data_concat['CO_ave2'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['CO_ave2'])[0][1])
+df['MOSx/CO_ave2'] = corrcoef_data
+
+print('MOS/O3_ave1')
+corrcoef_data = []
+for i in MOS: 
+    print(np.corrcoef(data_concat[i],data_concat['O3_ave1'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['O3_ave1'])[0][1])
+df['MOSx/O3_ave1'] = corrcoef_data
+
+print('MOS/O3_ave2')
+corrcoef_data = []
+for i in MOS: 
+    print(np.corrcoef(data_concat[i],data_concat['O3_ave2'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['O3_ave2'])[0][1])
+df['MOSx/O3_ave2'] = corrcoef_data
+
+print('MOS/HIH1_Av')
+corrcoef_data = []
+for i in MOS: 
+    print(np.corrcoef(data_concat[i],data_concat['HIH1_Av'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['HIH1_Av'])[0][1])
+df['MOSx/HIH1_Av'] = corrcoef_data
+
+print('MOS/LM65T1_Av')
+corrcoef_data = []
+for i in MOS: 
+    print(np.corrcoef(data_concat[i],data_concat['LM65T1_Av'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['LM65T1_Av'])[0][1])
+df['MOSx/LM65T1_Av'] = corrcoef_data
+
+#correlation between MOSc and other signals
+MOSc=[ 'MOS1c_Av','MOS2c_Av','MOS3c_Av','MOS4c_Av','MOS5c_Av','MOS6c_Av','MOS7c_Av','MOS8c_Av']
+print('MOSc/NO2_ave1')
+corrcoef_data = []
+for i in MOSc:   
+    print(np.corrcoef(data_concat[i],data_concat['NO2_ave1'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['NO2_ave1'])[0][1])
+df['MOScx/NO2_ave1'] = corrcoef_data
+
+print('MOSc/NO2_ave2')
+corrcoef_data = []
+for i in MOSc:   
+    print(np.corrcoef(data_concat[i],data_concat['NO2_ave2'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['NO2_ave2'])[0][1])
+df['MOScx/NO2_ave2'] = corrcoef_data
+
+print('MOSc/NO_ave1')
+corrcoef_data = []
+for i in MOSc: 
+    print(np.corrcoef(data_concat[i],data_concat['NO_ave1'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['NO_ave1'])[0][1])
+df['MOScx/NO_ave1'] = corrcoef_data
+
+print('MOSc/NO_ave2')
+corrcoef_data = []
+for i in MOSc: 
+    print(np.corrcoef(data_concat[i],data_concat['NO_ave2'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['NO_ave2'])[0][1])
+df['MOScx/NO_ave2'] = corrcoef_data
+
+print('MOSc/CO_ave1')
+corrcoef_data = []
+for i in MOSc: 
+    print(np.corrcoef(data_concat[i],data_concat['CO_ave1'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['CO_ave1'])[0][1])
+df['MOScx/CO_ave1'] = corrcoef_data
+
+print('MOSc/CO_ave2')
+corrcoef_data = []
+for i in MOSc: 
+    print(np.corrcoef(data_concat[i],data_concat['CO_ave2'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['CO_ave2'])[0][1])
+df['MOScx/CO_ave2'] = corrcoef_data
+
+print('MOSc/O3_ave1')
+corrcoef_data = []
+for i in MOSc: 
+    print(np.corrcoef(data_concat[i],data_concat['O3_ave1'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['O3_ave1'])[0][1])
+df['MOScx/O3_ave1'] = corrcoef_data
+
+print('MOSc/O3_ave2')
+corrcoef_data = []
+for i in MOSc: 
+    print(np.corrcoef(data_concat[i],data_concat['O3_ave2'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['O3_ave2'])[0][1])
+df['MOScx/O3_ave2'] = corrcoef_data
+
+print('MOSc/HIH1_Av')
+corrcoef_data = []
+for i in MOSc: 
+    print(np.corrcoef(data_concat[i],data_concat['HIH1_Av'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['HIH1_Av'])[0][1])
+df['MOScx/HIH1_Av'] = corrcoef_data
+
+print('MOSc/LM65T1_Av')
+corrcoef_data = []
+for i in MOSc: 
+    print(np.corrcoef(data_concat[i],data_concat['LM65T1_Av'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['LM65T1_Av'])[0][1])
+df['MOScx/LM65T1_Av'] = corrcoef_data
+
+#correlation between MOSb and other signals
+MOSb=[ 'MOS1b_Av','MOS2b_Av','MOS3b_Av','MOS4b_Av','MOS5b_Av','MOS6b_Av','MOS7b_Av','MOS8b_Av']
+print('MOSb/NO2_ave1')
+corrcoef_data = []
+for i in MOSb:   
+    print(np.corrcoef(data_concat[i],data_concat['NO2_ave1'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['NO2_ave1'])[0][1])
+df['MOSbx/NO2_ave1'] = corrcoef_data
+
+print('MOSb/NO2_ave2')
+corrcoef_data = []
+for i in MOSb:   
+    print(np.corrcoef(data_concat[i],data_concat['NO2_ave2'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['NO2_ave2'])[0][1])
+df['MOSbx/NO2_ave2'] = corrcoef_data
+
+print('MOSb/NO_ave1')
+corrcoef_data = []
+for i in MOSb: 
+    print(np.corrcoef(data_concat[i],data_concat['NO_ave1'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['NO_ave1'])[0][1])
+df['MOSbx/NO_ave1'] = corrcoef_data
+
+print('MOSb/NO_ave2')
+corrcoef_data = []
+for i in MOSb: 
+    print(np.corrcoef(data_concat[i],data_concat['NO_ave2'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['NO_ave2'])[0][1])
+df['MOSbx/NO_ave2'] = corrcoef_data
+
+print('MOSb/CO_ave1')
+corrcoef_data = []
+for i in MOSb: 
+    print(np.corrcoef(data_concat[i],data_concat['CO_ave1'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['CO_ave1'])[0][1])
+df['MOSbx/CO_ave1'] = corrcoef_data
+
+print('MOSb/CO_ave2')
+corrcoef_data = []
+for i in MOSb: 
+    print(np.corrcoef(data_concat[i],data_concat['CO_ave2'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['CO_ave2'])[0][1])
+df['MOSbx/CO_ave2'] = corrcoef_data
+
+print('MOSb/O3_ave1')
+corrcoef_data = []
+for i in MOSb: 
+    print(np.corrcoef(data_concat[i],data_concat['O3_ave1'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['O3_ave1'])[0][1])
+df['MOSbx/O3_ave1'] = corrcoef_data
+
+print('MOSb/O3_ave2')
+corrcoef_data = []
+for i in MOSb: 
+    print(np.corrcoef(data_concat[i],data_concat['O3_ave2'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['O3_ave2'])[0][1])
+df['MOSbx/O3_ave2'] = corrcoef_data
+
+print('MOSb/HIH1_Av')
+corrcoef_data = []
+for i in MOSb: 
+    print(np.corrcoef(data_concat[i],data_concat['HIH1_Av'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['HIH1_Av'])[0][1])
+df['MOSbx/HIH1_Av'] = corrcoef_data
+
+print('MOSb/LM65T1_Av')
+corrcoef_data = []
+for i in MOSb: 
+    print(np.corrcoef(data_concat[i],data_concat['LM65T1_Av'])[0][1])
+    corrcoef_data.append(np.corrcoef(data_concat[i], data_concat['LM65T1_Av'])[0][1])
+df['MOSbx/LM65T1_Av'] = corrcoef_data
+
+"""MOSb=[ 'MOS1b_Av','MOS2b_Av','MOS3b_Av','MOS4b_Av','MOS5b_Av','MOS6b_Av','MOS7b_Av','MOS8b_Av']
+print('MOSb/VOC')
+for i in MOSb:   
+    print(np.corrcoef(data_concat[i],data_concat['VOC'])[0][1])
+   
+print('MOSb/VOC')
+for i in MOSb:   
+    print(np.corrcoef(data_concat[i],data_concat['VOC'])[0][1])
+   
+print('MOSb/VOC')
+for i in MOSb: 
+    print(np.corrcoef(data_concat[i],data_concat['VOC'])[0][1])
+
+print('MOSb/VOC')
+for i in MOSb: 
+    print(np.corrcoef(data_concat[i],data_concat['VOC'])[0][1])
+
+print('MOSb/VOC')
+for i in MOSb: 
+    print(np.corrcoef(data_concat[i],data_concat['VOC'])[0][1])
+
+print('MOSb/VOC')
+for i in MOSb: 
+    print(np.corrcoef(data_concat[i],data_concat['VOC'])[0][1])
+
+print('MOSb/VOC')
+for i in MOSb: 
+    print(np.corrcoef(data_concat[i],data_concat['VOC'])[0][1])
+
+print('MOSb/VOC')
+for i in MOSb: 
+    print(np.corrcoef(data_concat[i],data_concat['VOC'])[0][1])"""
+
+
+temp1 = matrix(data_concat['MOS1b_Av'][1:]) - matrix(data_concat['MOS1b_Av'][0:-1])
+temp1 = pd.DataFrame(temp1)
+temp2 = matrix(data_concat['MOS1c_Av'][1:]) - matrix(data_concat['MOS1c_Av'][0:-1])
+temp2 = pd.DataFrame(temp2)
+temp3 = matrix(data_concat['HIH1_Av'][1:]) - matrix(data_concat['HIH1_Av'][0:-1])
+temp3 = pd.DataFrame(temp3)
+print(np.corrcoef(data_concat['MOS1b_Av'],data_concat['MOS1c_Av']))
+print(np.corrcoef(data_concat['MOS1_Av'][1:],temp3))
+print(np.corrcoef(temp1, data_concat['HIH1_Av'][1:]))
+
+data_voc = voc_reader.extract_voc('D:/WACL/Data/', 'Detailed Compound Concentrations', 'Analyte vs Time') 
+data_merge = data_concat.merge(data_voc, how = 'inner', on = ['Time'])
+
+VOC = ['CH5O+ (methanol;H3O+) (ppb)',	'CH3CN.H+ (acetonitrile;H3O+) (ppb)',	'C3H7O+ (acetone;H3O+) (ppb)',	'C4H6O.H+ (3-buten-2-one;H3O+) (ppb)',	'C6H6.H+ (benzene;H3O+) (ppb)',	'C8H10.H+ (m-xylene;H3O+) (ppb)',	'C9H12.H+ (1,2,4-trimethylbenzene;H3O+) (ppb)',	'H3O+.C8H18 (octane;H3O+) (ppb)',	'C9H20.H3O+ (nonane;H3O+) (ppb)',	'H3O+.C10H22 (decane;H3O+) (ppb)',	'CH3O+ (formaldehyde;H3O+) (ppb)',	'NO+.C3H6O (acetone;NO+) (ppb)',	'C5H8+ (isoprene;NO+) (ppb)',	'C4H6O.NO+ (3-buten-2-one;NO+) (ppb)',	'NO+.C4H8O (butanone;NO+) (ppb)',	'C6H6+ (benzene;NO+) (ppb)',	'NO.C6H6+ (benzene;NO+) (ppb)',	'C7H8+ (toluene;NO+) (ppb)',	'C8H10+ (m-xylene;NO+) (ppb)',	'C9H12+ (1,2,4-trimethylbenzene;NO+) (ppb)',	'C4H6+ (1,3-butadiene;NO+) (ppb)',	'C8H17+ (octane;NO+) (ppb)',	'C10H21+ (decane;NO+) (ppb)',	'C2H5O+ (ethanol;NO+) (ppb)',	'C3H6O+ (acetone;O2+) (ppb)',	'C5H7+ (isoprene;O2+) (ppb)',	'C5H8+ (isoprene;O2+) (ppb)',	'C4H8O+ (butanone;O2+) (ppb)',	'C6H6+ (benzene;O2+) (ppb)',	'C7H8+ (toluene;O2+) (ppb)',	'C7H7+ (m-xylene;O2+) (ppb)',	'C8H10+ (m-xylene;O2+) (ppb)',	'C9H12+ (1,2,4-trimethylbenzene;O2+) (ppb)',	'C3H3+ (1,3-butadiene;O2+) (ppb)',	'C4H6+ (1,3-butadiene;O2+) (ppb)',	'C8H18+ (octane;O2+) (ppb)',	'C10H22+ (decane;O2+) (ppb)',	'C2H5O+ (ethanol;O2+) (ppb)',	'C2H6O+ (ethanol;O2+) (ppb)'
+]
+voc_corr = []
+
+for i in VOC:
+    print(i)
+    print(np.corrcoef(data_merge['HIH1_Av'],data_merge[i])[0][1])
+for i, j, k in zip(MOS, MOSb, MOSc):
+    print(i)
+    print(np.corrcoef(data_merge['HIH1_Av'],data_merge[i])[0][1])
+    print(j)
+    print(np.corrcoef(data_merge['HIH1_Av'],data_merge[j])[0][1])
+    print(k)
+    print(np.corrcoef(data_merge['HIH1_Av'],data_merge[k])[0][1])
+
+for i in VOC:
+    print(i + '/MOS1c_Av')
+    voc_corr.append(i + '/MOS1c_Av')
+    print(np.corrcoef(data_merge['MOS1c_Av'],data_merge[i])[0][1])
+    voc_corr.append(np.corrcoef(data_merge['MOS1c_Av'],data_merge[i])[0][1])
+
+voc_df = pd.DataFrame(voc_corr)
+voc_df.to_csv('voc_corr.csv')
+
+"""with open('filename.csv', 'w', newline='') as myfile:
+    wr = csv.writer(myfile,, quoting=csv.QUOTE_ALL)
+    wr.writerow(voc_corr)
+df.to_csv('coef.csv')"""
