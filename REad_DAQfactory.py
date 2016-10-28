9b70e3dea0d0c25df75eb477b74c94c47df6b1c8
lldcl <daichengliang@gmail.com>
2016-10-28 15:24:55 +0100

    path

--- REad_DAQfactory.py
+++ REad_DAQfactory.py
@@ -16,9 +16,9 @@ from scipy import stats
 from matplotlib.offsetbox import AnchoredText
 
 # The path to where the raw files are stored
-path = "..\Data\wacl_data\Raw_data_files\\"
+path = "../Data/wacl_data/Raw_data_files/"
 f_date = '201610'
-cal_file = os.listdir(path + f_date +'\MOS')
+cal_file = os.listdir(path + f_date +'/MOS')
 # The name of the MOS file to be analysed
 data_concat = mr.readin(path, f_date, cal_file, 1, 5)
 
