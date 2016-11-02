54d48f0afb1952ba3728ca1aa0932bcc6d73b705
lldcl <daichengliang@gmail.com>
2016-11-02 12:49:09 +0000

    mis

--- REad_DAQfactory.py
+++ REad_DAQfactory.py
@@ -206,12 +206,11 @@ for n,c in zip(MOS,colors):
     plt.setp(ltext, fontsize='large')    # the legend text fontsize
     plt.ylabel("Humidity", size=20)
     plt.xlabel("Time", size=20)	
-
 Hmdfig.show()
 
 sub = 'vocs1'
-voc1=[ 'CH5O+ (methanol;H3O+) (ppb)',	'CH3CN.H+ (acetonitrile;H3O+) (ppb)',	'C3H7O+ (acetone;H3O+) (ppb)',	'C4H6O.H+ (3-buten-2-one;H3O+) (ppb)',	'C6H6.H+ (benzene;H3O+) (ppb)',	'C8H10.H+ (m-xylene;H3O+) (ppb)',	'C9H12.H+ (1,2,4-trimethylbenzene;H3O+) (ppb)',	'H3O+.C8H18 (octane;H3O+) (ppb)',	'C9H20.H3O+ (nonane;H3O+) (ppb)',	'H3O+.C10H22 (decane;H3O+) (ppb)',	'CH3O+ (formaldehyde;H3O+) (ppb)',	'NO+.C3H6O (acetone;NO+) (ppb)','MOS1c_Av']
-VOCs1fig = plt.figure("CH5O+ (methanol;H3O+) (ppb)")
+voc1=[ 'CH5O+ (methanol;H3O+) (ppb)',	'CH3CN.H+ (acetonitrile;H3O+) (ppb)',	'C3H7O+ (acetone;H3O+) (ppb)',	'C4H6O.H+ (3-buten-2-one;H3O+) (ppb)',	'C6H6.H+ (benzene;H3O+) (ppb)',	'C8H10.H+ (m-xylene;H3O+) (ppb)',	'C9H12.H+ (1,2,4-trimethylbenzene;H3O+) (ppb)',	'H3O+.C8H18 (octane;H3O+) (ppb)',	'C9H20.H3O+ (nonane;H3O+) (ppb)',	'H3O+.C10H22 (decane;H3O+) (ppb)',	'CH3O+ (formaldehyde;H3O+) (ppb)',	'MOS1c_Av']
+VOCs1fig = plt.figure("voc1")
 ax1 = VOCs1fig.add_subplot(111)
 colors = ["black","firebrick", "lightgreen" , "c", "darkblue", "purple","orange","forestgreen", "lightskyblue" , "indigo", "dimgrey", "fuchsia"]
 for n,c in zip(voc1,colors):
@@ -223,61 +222,97 @@ for n,c in zip(voc1,colors):
     leg = plt.gca().get_legend()
     ltext  = leg.get_texts()  # all the text.Text instance in the legend
     plt.setp(ltext, fontsize='large')    # the legend text fontsize
-    plt.ylabel("vocs1/MOSc*150", size=20)
+    plt.ylabel("vocs1/MOSc*15", size=20)
     plt.xlabel("Time", size=20)	
 VOCs1fig.show()
 
 
 sub = 'vocs2'
-voc2=[ 'C5H8+ (isoprene;NO+) (ppb)',	'C4H6O.NO+ (3-buten-2-one;NO+) (ppb)',	'NO+.C4H8O (butanone;NO+) (ppb)',	'C6H6+ (benzene;NO+) (ppb)',	'NO.C6H6+ (benzene;NO+) (ppb)',	'C7H8+ (toluene;NO+) (ppb)',	'C8H10+ (m-xylene;NO+) (ppb)',	'C9H12+ (1,2,4-trimethylbenzene;NO+) (ppb)',	'C4H6+ (1,3-butadiene;NO+) (ppb)', 'MOS1c_Av']
+voc2=[ 'NO+.C3H6O (acetone;NO+) (ppb)','C5H8+ (isoprene;NO+) (ppb)',	'C4H6O.NO+ (3-buten-2-one;NO+) (ppb)',	'NO+.C4H8O (butanone;NO+) (ppb)',	'C6H6+ (benzene;NO+) (ppb)',	'NO.C6H6+ (benzene;NO+) (ppb)',	'C7H8+ (toluene;NO+) (ppb)',	'C8H10+ (m-xylene;NO+) (ppb)',	'C9H12+ (1,2,4-trimethylbenzene;NO+) (ppb)',	'C4H6+ (1,3-butadiene;NO+) (ppb)', 'MOS1c_Av']
 VOCs2fig = plt.figure("vocs2")
 ax1 = VOCs2fig.add_subplot(111)
 colors = ["black","firebrick", "lightgreen" , "c", "darkblue", "purple","orange","forestgreen", "lightskyblue" , "indigo", "dimgrey", "fuchsia"]
 for n,c in zip(voc2,colors):
     if n == 'MOS1c_Av':
-        ax1.plot(data_merge.Time,data_merge[n]*100,color=c,linewidth=3)
+        ax1.plot(data_merge.Time,data_merge[n]*15,color=c,linewidth=3)
     else:
         ax1.plot(data_merge.Time,data_merge[n],color=c,linewidth=3)
     plt.legend(voc2, bbox_to_anchor=(0., 1.02, 1., .102),ncol=5, loc=2, mode="expand", borderaxespad=0.)
     leg = plt.gca().get_legend()
     ltext  = leg.get_texts()  # all the text.Text instance in the legend
     plt.setp(ltext, fontsize='large')    # the legend text fontsize
-    plt.ylabel("vocs2/MOSc*150", size=20)
+    plt.ylabel("vocs2/MOSc*15", size=20)
     plt.xlabel("Time", size=20)	
 VOCs2fig.show()
 
 sub = 'vocs3'
-voc3=[ 'C8H17+ (octane;NO+) (ppb)',	'C10H21+ (decane;NO+) (ppb)',	'C2H5O+ (ethanol;NO+) (ppb)',	'C3H6O+ (acetone;O2+) (ppb)',	'C5H7+ (isoprene;O2+) (ppb)',	'C5H8+ (isoprene;O2+) (ppb)',	'C4H8O+ (butanone;O2+) (ppb)',	'C6H6+ (benzene;O2+) (ppb)',	'C7H8+ (toluene;O2+) (ppb)',	'C7H7+ (m-xylene;O2+) (ppb)', 'MOS1c_Av']
+voc3=[ 'C8H17+ (octane;NO+) (ppb)',	'C10H21+ (decane;NO+) (ppb)', 'C3H6O+ (acetone;O2+) (ppb)',	'C5H7+ (isoprene;O2+) (ppb)',	'C5H8+ (isoprene;O2+) (ppb)',	'C4H8O+ (butanone;O2+) (ppb)',	'C6H6+ (benzene;O2+) (ppb)',	'C7H8+ (toluene;O2+) (ppb)',	'C7H7+ (m-xylene;O2+) (ppb)', 'MOS1c_Av']
 VOCs3fig = plt.figure("vocs3")
 ax1 = VOCs3fig.add_subplot(111)
 colors = ["black","firebrick", "lightgreen" , "c", "darkblue", "purple","orange","forestgreen", "lightskyblue" , "indigo", "dimgrey", "fuchsia"]
 for n,c in zip(voc3,colors):
     if n == 'MOS1c_Av':
-        ax1.plot(data_merge.Time,data_merge[n]*100,color=c,linewidth=3)
+        ax1.plot(data_merge.Time,data_merge[n]*15,color=c,linewidth=3)
     else:
         ax1.plot(data_merge.Time,data_merge[n],color=c,linewidth=3)
     plt.legend(voc3, bbox_to_anchor=(0., 1.02, 1., .102),ncol=5, loc=2, mode="expand", borderaxespad=0.)
     leg = plt.gca().get_legend()
     ltext  = leg.get_texts()  # all the text.Text instance in the legend
     plt.setp(ltext, fontsize='large')    # the legend text fontsize
-    plt.ylabel("vocs3/MOSc*150", size=20)
+    plt.ylabel("vocs3/MOSc*15", size=20)
     plt.xlabel("Time", size=20)	
 VOCs3fig.show()
 
 sub = 'vocs4'
-voc4=[ 'C8H10+ (m-xylene;O2+) (ppb)',	'C9H12+ (1,2,4-trimethylbenzene;O2+) (ppb)',	'C3H3+ (1,3-butadiene;O2+) (ppb)',	'C4H6+ (1,3-butadiene;O2+) (ppb)',	'C8H18+ (octane;O2+) (ppb)',	'C10H22+ (decane;O2+) (ppb)',	'C2H5O+ (ethanol;O2+) (ppb)',	'C2H6O+ (ethanol;O2+) (ppb)', 'MOS1c_Av']
+voc4=[ 'C8H10+ (m-xylene;O2+) (ppb)',	'C9H12+ (1,2,4-trimethylbenzene;O2+) (ppb)',	'C4H6+ (1,3-butadiene;O2+) (ppb)',	'C8H18+ (octane;O2+) (ppb)',	'C10H22+ (decane;O2+) (ppb)',	'C2H6O+ (ethanol;O2+) (ppb)', 'MOS1c_Av']
 VOCs4fig = plt.figure("vocs4")
 ax1 = VOCs4fig.add_subplot(111)
 colors = ["black","firebrick", "lightgreen" , "c", "darkblue", "purple","orange","forestgreen", "lightskyblue" , "indigo", "dimgrey", "fuchsia"]
 for n,c in zip(voc4,colors):
     if n == 'MOS1c_Av':
-        ax1.plot(data_merge.Time,data_merge[n]*100,color=c,linewidth=3)
+        ax1.plot(data_merge.Time,data_merge[n]*15,color=c,linewidth=3)
     else:
         ax1.plot(data_merge.Time,data_merge[n],color=c,linewidth=3)
     plt.legend(voc4, bbox_to_anchor=(0., 1.02, 1., .102),ncol=5, loc=2, mode="expand", borderaxespad=0.)
     leg = plt.gca().get_legend()
     ltext  = leg.get_texts()  # all the text.Text instance in the legend
     plt.setp(ltext, fontsize='large')    # the legend text fontsize
-    plt.ylabel("vocs4/MOSc*150", size=20)
+    plt.ylabel("vocs4/MOSc*15", size=20)
+    plt.xlabel("Time", size=20)	
+VOCs4fig.show()
+
+sub = 'vocs5'
+voc5 = ['C2H5O+ (ethanol;NO+) (ppb)',  'C2H5O+ (ethanol;O2+) (ppb)', 'MOS1c_Av']
+VOCs5fig = plt.figure("vocs5")
+ax1 = VOCs5fig.add_subplot(111)
+colors = ["black","firebrick", "lightgreen" , "c", "darkblue", "purple","orange","forestgreen", "lightskyblue" , "indigo", "dimgrey", "fuchsia"]
+for n,c in zip(voc5,colors):
+    if n == 'MOS1c_Av':
+        ax1.plot(data_merge.Time,data_merge[n]*100,color=c,linewidth=3)
+    else:
+        ax1.plot(data_merge.Time,data_merge[n],color=c,linewidth=3)
+    plt.legend(voc5, bbox_to_anchor=(0., 1.02, 1., .102),ncol=5, loc=2, mode="expand", borderaxespad=0.)
+    leg = plt.gca().get_legend()
+    ltext  = leg.get_texts()  # all the text.Text instance in the legend
+    plt.setp(ltext, fontsize='large')    # the legend text fontsize
+    plt.ylabel("vocs5/MOSc*100", size=20)
+    plt.xlabel("Time", size=20)	
+VOCs5fig.show()
+
+sub = 'vocs6'
+voc6 = ['C3H3+ (1,3-butadiene;O2+) (ppb)', 'MOS1c_Av']
+VOCs6fig = plt.figure("vocs6")
+ax1 = VOCs6fig.add_subplot(111)
+colors = ["black","firebrick", "lightgreen" , "c", "darkblue", "purple","orange","forestgreen", "lightskyblue" , "indigo", "dimgrey", "fuchsia"]
+for n,c in zip(voc6,colors):
+    if n == 'MOS1c_Av':
+        ax1.plot(data_merge.Time,data_merge[n]*300,color=c,linewidth=3)
+    else:
+        ax1.plot(data_merge.Time,data_merge[n],color=c,linewidth=3)
+    plt.legend(voc6, bbox_to_anchor=(0., 1.02, 1., .102),ncol=5, loc=2, mode="expand", borderaxespad=0.)
+    leg = plt.gca().get_legend()
+    ltext  = leg.get_texts()  # all the text.Text instance in the legend
+    plt.setp(ltext, fontsize='large')    # the legend text fontsize
+    plt.ylabel("vocs6/MOSc*300", size=20)
     plt.xlabel("Time", size=20)	
 plt.show()
